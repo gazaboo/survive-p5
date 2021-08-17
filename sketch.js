874bf0435f6437fa3ed543c108b80d662ad68d5e
Florian Dadouchi <florian.dadouchi@gmail.com>
2021-08-17 16:37:36 +0200

    fix bind

--- sketch.js
+++ sketch.js
@@ -1,60 +1,53 @@
-let game;
+let game; 
+let interval = 1000;
+
+
 
 function setup() {
   createCanvas(640,480)
   game = new Game(); 
-  setInterval(game.add_virus, 1000);  
+  setInterval(game.add_adversaire.bind(game), interval);     
 }
 
 
 function draw() {
   background(20, 100, 20);
-  game.play()
-
-
-  // if (p.isAlive){
-  //   v.display(); 
-  //   v.move(); 
-  //   p.display(); 
-  //   p.move();
-  //   v.testCollision(p);    
-  // } else {
-  //   textSize(30)
-  //   text("T'es pas assez en forme \npour continuer à jouer", 200, 200);
-  // }
-
+  
+  if (game.player.isAlive){
+    game.play(); 
+  } else {
+    textSize(30)
+    text("T'es pas assez en forme \npour continuer à jouer", 200, 200);
+  }
 }
 
+
+
 class Game {
-  
   constructor(){
-    this.pandemie = []
-    this.citoyen = new Player();   
+    this.adversaires = []
+    this.player = new Player();
   }
 
-  add_virus(){
-    let new_virus = new Virus();
-    this.pandemie.push(new_virus);  
+  add_adversaire(){ 
+    this.adversaires.push(new Adversaire()); 
   }
 
   play(){
-    this.citoyen.display(); 
-    this.citoyen.move()
+    this.player.display(); 
+    this.player.move()
 
-    this.pandemie.forEach( v => {
-      v.display();
-      v.move();  
+    this.adversaires.forEach( adversaire => {
+      adversaire.display();
+      adversaire.move();  
+      adversaire.testCollision(this.player);
     }); 
-
   }
-
-
-
 }
 
-class Virus {
+class Adversaire {
   constructor(){
-    this.d = 40; 
+    this.size = 40; 
     this.x = random(20, 620); 
     this.y = random(20, 460);
     this.direction = random([-1, 1]); 
@@ -63,7 +56,7 @@ class Virus {
   
   display(){
     fill('red')
-    circle(this.x, this.y, this.d); 
+    circle(this.x, this.y, this.size); 
     fill('white')
   }
 
@@ -74,10 +67,10 @@ class Virus {
     }  
   }
 
-  testCollision(p){
-    let distance = dist(this.x, this.y, p.x, p.y )
-    if (distance < (this.d + p.d)/2){
-      p.isAlive = false;     
+  testCollision(player){
+    let distance = dist(this.x, this.y, player.x, player.y )
+    if (distance < (this.size + player.size)/2){
+      player.isAlive = false;     
     } 
   }
 }
@@ -88,12 +81,12 @@ class Player {
   constructor(){
     this.x = 100; 
     this.y = 100;
-    this.d = 80; 
+    this.size = 80; 
     this.isAlive = true;   
   }
   
   display(){
-    circle(this.x, this.y, this.d); 
+    circle(this.x, this.y, this.size); 
   }
 
   move(){
@@ -107,5 +100,4 @@ class Player {
     if( this.y > 440){ this.y = 440 }
     if( this.y < 40 ){ this.y = 40 }
   }
-
 }
